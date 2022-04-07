2ce0c9b2cf5c4099fa461d186b41fe723e86283b
Dino Horvat <dino.horvat985@gmail.com>
2022-04-07 23:22:52 +0200

    Dodavanje svih potrebnih datoteka

---
+++ js/vlbdata1.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({borderSize:57,classNames:'lightbox',closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:false,resizeSpeed:7,slideTime:4,startZoom:true}) });
